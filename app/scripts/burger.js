563e4122cdcb4ef453405551adc470d3935c044f
Dustin Hayes <dhayes@weblinc.com>
2015-04-07 09:56:12 -0400

    refactor to work with lastest release

--- app/scripts/burger.js
+++ app/scripts/burger.js
@@ -6,10 +6,9 @@
   var burgerNav = document.getElementsByClassName('burger-nav')[0];
   var burgerBrand = document.getElementsByClassName('burger-brand')[0];
 
-  burgerContain.onclick = function() {
-    body.classList.toggle('open');
-    burgerContain.classList.toggle('open');
-    burgerNav.classList.toggle('open');
-    burgerBrand.classList.toggle('open');
-  };
+  burgerContain.addEventListener('click', function toggleClasses() {
+    [body, burgerContain, burgerNav, burgerBrand].forEach(function (el) {
+      el.classList.toggle('open');
+    });
+  }, false);
 })();
